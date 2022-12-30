c6cc8f5659e5289b2a02040148e6750db94b41d4
katanakiwi <katanakiwi@gmail.com>
2022-12-30 16:49:57 +0100

    finished day 12a. Actually proud of myself!

--- 12.ps1
+++ 12.ps1
@@ -1,6 +1,6 @@
 set-location "$PSScriptRoot"
 $inputFile = "12sample.txt"
-#$inputFile = "12.txt"
+$inputFile = "12.txt"
 
 $lines = Get-Content $inputFile
 
@@ -65,15 +65,18 @@ foreach($i in (0..($lines.Length-1))) {
         $index = @{}
         $index.x = $j
         $index.y = $i
+        $index.drawIcon = "."
         $index.letter = $lines[$i][$j]
+        $index.parent = $false
 
         if($index.letter -ceq "S") {
             write-host "Start at $i,$j"
-            $start = "$i,$j"
+            $start = "$j,$i"
             $index.letter = "a"
+            $index.parent = "start"
         } elseif ($index.letter -ceq "E") {
             write-host "End at $i,$j"
-            $end = "$i,$j"
+            $end = "$j,$i"
             $index.letter = "z"
         }
 
@@ -88,7 +91,84 @@ foreach($index in $hashTable.keys) {
     $hashtable[$index].validMoves = findValidMoves $hashTable["$index"] $hashTable
 }
 
-<#
-todo implement graph
-implement algorithm? Dijkstra, A*, breadth-first?
-#>
+$queue = @()
+$queue += $hashTable[$start]
+$result = @()
+
+while($queue.Length -gt 0) {
+    #dequeue first in 
+    $currentVex = $queue[0]
+    if($queue.Length -gt 1) {
+        $queue = $queue[1..$($queue.Length-1)]
+    } else { $queue = @() }
+    
+    if(-not($currentVex.visited)) { #if currentVex not visited yet
+        $xt = $currentVex.x
+        $yt = $currentVex.y
+        $hashTable["$xt,$yt"].visited = $true
+        $result += $currentVex
+
+        #add edges to queue
+        if($currentVex.validMoves.U) {
+            $var = "$($xt),$($yt-1)"
+            if(-not($hashTable["$var"].visited)){
+                $queue += $hashTable["$var"]
+                if(-not($hashTable["$var"].parent)) {
+                    $hashTable["$var"].parent = $currentVex
+                }
+            }
+        }
+        if($currentVex.validMoves.D) {
+            $var = "$($xt),$($yt+1)"
+            if(-not($hashTable["$var"].visited)){
+                $queue += $hashTable["$var"]
+                if(-not($hashTable["$var"].parent)) {
+                    $hashTable["$var"].parent = $currentVex
+                }
+            }
+        }
+        if($currentVex.validMoves.L) {
+            $var = "$($xt-1),$($yt)"
+            if(-not($hashTable["$var"].visited)){
+                $queue += $hashTable["$var"]
+                if(-not($hashTable["$var"].parent)) {
+                    $hashTable["$var"].parent = $currentVex
+                }
+            }
+        }
+        if($currentVex.validMoves.R) {
+            $var = "$($xt+1),$($yt)"
+            if(-not($hashTable["$var"].visited)){
+                $queue += $hashTable["$var"]
+                if(-not($hashTable["$var"].parent)) {
+                    $hashTable["$var"].parent = $currentVex
+                }
+            }
+        }
+    }
+}
+
+function drawMap {
+    Write-Host ""
+    for($t=0; $t -lt $height; $t++) {
+        $output = ""
+        for($s=0; $s -lt $width; $s++) {
+            $output += $hashTable["$s,$t"].drawIcon
+        }
+        write-host $output
+    }
+    Write-Host ""
+}
+
+write-host "finding shortest path..."
+$pathlength = 0
+$revPath = $hashTable[$end]
+while($($revPath.parent) -ne "start") {
+    $pathLength += 1
+    $hashTable["$($revPath.x),$($revPath.y)"].drawIcon = "X"
+    $revPath = $revPath.parent
+    #drawMap
+}
+$hashTable["$start"].drawIcon = "X"
+drawmap
+write-host "Pathlength: $pathlength"
